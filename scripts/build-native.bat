bb4b6de14a4e8b66633dadb6c34c722f50f7172e
LahkLeKey <LahkLeKey@gmail.com>
2026-04-13 23:40:38 -0500

    launch profile update

--- scripts/build-native.bat
+++ scripts/build-native.bat
@@ -6,6 +6,9 @@ set BUILD_DIR=%ROOT_DIR%\build\native
 
 if not exist "%BUILD_DIR%" mkdir "%BUILD_DIR%"
 
+if exist "%BUILD_DIR%\CMakeCache.txt" del /q "%BUILD_DIR%\CMakeCache.txt"
+if exist "%BUILD_DIR%\CMakeFiles" rmdir /s /q "%BUILD_DIR%\CMakeFiles"
+
 cmake -S "%ROOT_DIR%" -B "%BUILD_DIR%" -G "Visual Studio 17 2022" -A x64
 if errorlevel 1 exit /b 1
 
